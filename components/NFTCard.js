7398f33dc2252e41bb6fdb23bd0a220d995a5671
Alberto <alberto.moreno2357@gmail.com>
2024-03-08 17:44:18 +0100

    feat: styling nft

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,9 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image, Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
+import { COLORS, SIZES, SHADOWS, assets} from '../constants'
 
 const NFTCard = () => {
+  const navigation = useNavigation()
+
   return (
-    <View>
+    <View style={{
+      backgroundColor: COLORS.white,
+      borderRadius: SIZES.font,
+      marginBottom: SIZES.extraLarge,
+      margin: SIZES.base,
+      ...SHADOWS.dark
+    }}>
       <Text>NFTCard</Text>
     </View>
   )
--- screens/Home.js
+++ screens/Home.js
@@ -3,6 +3,7 @@ import { View, SafeAreaView, FlatList, Text } from 'react-native';
 
 import { COLORS, NFTData} from '../constants';
 import { FocusedStatusBar, NFTCard, HomeHeader } from '../components';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const Home = () => {
   return (
@@ -12,11 +13,23 @@ const Home = () => {
         <View style={{zIndex: 0}}>
           <FlatList 
             data={NFTData}
-            renderItem={({item}) => <Text>{item.name}</Text>}
+            renderItem={({item}) => <NFTCard data={item}/>}
             keyExtractor={(item) => item.id}
             showsVerticalScrollIndicator={false}
             ListHeaderComponent={<HomeHeader />}
           />
+        </View>
+
+        <View style={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          zIndex: -1,
+        }}>
+          <View style={{ height: 300, backgroundColor: COLORS.primary}}/>
+          <View style={{ flex: 1, backgroundColor: COLORS.white}}/>
         </View>
       </View>
     </SafeAreaView>
