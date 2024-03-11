e73763e5e72521ad4cc0af326016142afad6235d
Alberto <alberto.moreno2357@gmail.com>
2024-03-11 22:10:36 +0100

    feat: adding Description

--- components/index.js
+++ components/index.js
@@ -1,5 +1,9 @@
 import HomeHeader from "./HomeHeader";
 import FocusedStatusBar from "./FocusedStatusBar";
 import NFTCard from "./NFTCard";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import {  RectButton, CircleButton } from './Button';
+import { SubInfo } from './SubInfo'
 
-export { HomeHeader, FocusedStatusBar, NFTCard };
+export { HomeHeader, FocusedStatusBar, NFTCard, DetailsDesc, DetailsBid, RectButton, CircleButton, SubInfo};
